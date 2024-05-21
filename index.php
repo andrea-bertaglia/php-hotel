6877b8cc6e07aaea9caaf02798d19adc85b272fa
andrea-bertaglia <andreabertaglia@gmail.com>
2024-05-21 17:34:10 +0200

    feat: print hotels in table layout

--- index.php
+++ index.php
@@ -39,11 +39,11 @@ $hotels = [
 ];
 // var_dump($hotels);
 
-foreach ($hotels as $cur_elem) {
-    foreach ($cur_elem as $key => $content) {
-        echo "<p>{$key}: {$content}";
-    }
-}
+// foreach ($hotels as $cur_elem) {
+//     foreach ($cur_elem as $key => $content) {
+//         echo "<p>{$key}: {$content}";
+//     }
+// }
 ?>
 
 <!DOCTYPE html>
@@ -54,9 +54,57 @@ foreach ($hotels as $cur_elem) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
 
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 
 <body>
+
+    <h1 class="text-center mt-3 mb-5 fw-bold">Elenco Hotels</h1>
+
+    <div class="container">
+
+        <table class="table table-primary table-striped-columns">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Parcheggio</th>
+                    <th scope="col">Voto</th>
+                    <th scope="col">Distanza dal centro</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- ciclo tutti gli elementi dell'array e prelevo anche l'index per usarlo come identificativo della row della table -->
+                <?php foreach ($hotels as $index => $cur_elem) { ?>
+                    <?php $index++ ?>
+                    <tr>
+                        <th scope="row"><?php echo $index ?></th>
+
+                        <!-- determino se è presente il parcheggio e assegno l'icona (emoji) alla variabile -->
+                        <?php
+                        $parking;
+                        if ($cur_elem["parking"]) {
+                            $parking = "✅";
+                        } else {
+                            $parking = "❌";
+                        }
+                        ?>
+
+                        <?php echo "<td>{$cur_elem["name"]}</td>" ?>
+                        <?php echo "<td>{$cur_elem["description"]}</td>" ?>
+                        <?php echo "<td>$parking</td>" ?>
+                        <?php echo "<td>{$cur_elem["vote"]}</td>" ?>
+                        <?php echo "<td>{$cur_elem["distance_to_center"]} km</td>" ?>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+
+
+    </div>
 
 </body>
 
